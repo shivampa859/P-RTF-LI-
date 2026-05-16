13c20fa0e0ddb3a172d938de61548434408ac4b6
Shivam <185154391+Devl8-hub@users.noreply.github.com>
2026-05-16 22:38:44 +0530

    fixed deployment

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,3 +137,5 @@ EMAIL_HOST_USER = 'shivampatel939957@gmail.com'
 EMAIL_HOST_PASSWORD = 'gcfs kluq ktni jrut'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
